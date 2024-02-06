42689bb8bb78ebdb6ef7ecdec09698a1512f38b6
Михаил <dev_velikanov@fiftygames.ru>
2024-02-06 16:52:36 +0500

    количество вопросов в категориях. Версия 1.2.0

--- main/quiz.js
+++ main/quiz.js
@@ -8,24 +8,40 @@ class QuizManager {
 
     static async quiz(category, multiple, difficulty) {
         category = category ? category : Math.floor(Math.random() * items.cat.length);
-        multiple = multiple ? multiple==="true" : true/*items.multiple[Math.floor(Math.random() * items.multiple.length)]*/;
+        multiple = multiple ? multiple === "true" : true/*items.multiple[Math.floor(Math.random() * items.multiple.length)]*/;
         difficulty = difficulty ? difficulty : Math.floor(Math.random() * items.diff.length);
 
-        if(!items.cat[category] || !items.diff[difficulty]) return error(404, `Категория или сложность не найдены!`)
-        if(typeof(multiple) != 'boolean') return error(400, `multiple может принимать только булевые значения!`)
+        if (!items.cat[category] || !items.diff[difficulty]) return error(404, `Категория или сложность не найдены!`)
+        if (typeof (multiple) != 'boolean') return error(400, `multiple может принимать только булевые значения!`)
 
         let quizes = await Quiz.find({ category: category, multiple: multiple, difficulty: difficulty })
-        if(quizes.length < 1) return error(404, `Вопросов по данному запросу не найдено!`)
+        if (quizes.length < 1) return error(404, `Вопросов по данному запросу не найдено!`)
         return getRandomElements(quizes, 1)
     }
 
-    static async create(key, category, multiple, difficulty, question, correct_answer, incorrect_answers)  {
-        if(!key || key != config.key) return error(403, `Нет доступа`)
+    static async create(key, category, multiple, difficulty, question, correct_answer, incorrect_answers) {
+        if (!key || key != config.key) return error(403, `Нет доступа`)
         await Quiz.create({ category, multiple, difficulty, question, correct_answer, incorrect_answers }).then(() => {
             return { "created": "true" };
         }).catch(() => {
             return { "created": "false" };
         })
+    }
+
+    static async list() {
+        let data = await Quiz.find({});
+
+        const categoryCounts = {};
+
+        data.forEach(elem => {
+            if (!categoryCounts[elem.category]) {
+                categoryCounts[elem.category] = 1;
+            } else {
+                categoryCounts[elem.category]++;
+            }
+        });
+
+        return categoryCounts
     }
 }
 
